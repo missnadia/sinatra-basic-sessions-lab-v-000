b35fed3ff46aa5c0d61d58dfb73171981cc63082
Nadia Lee <miss.nadia.lee@gmail.com>
2018-04-12 19:52:52 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,8 +6,13 @@ class App < Sinatra::Base
     enable :sessions
     set :session_secret, "secret"
   end
-  
+
   get '/' do
     erb :index
+  end
+
+  get '/checkout' do
+    session["item"] = user
+    @session = session
   end
 end
